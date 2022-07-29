73295011d6af11f338d83b1f3144fd8476ae24ea
alimusa2809 <ali.musa.28.09@gmail.com>
2022-07-29 10:14:30 +0500

    Update models.py

--- datingApp/models.py
+++ datingApp/models.py
@@ -114,3 +114,14 @@ class Interests(models.Model):
 
     def __str__(self):
         return self.interestsChoice
+
+
+class MatchMake(models.Model):
+    person1 = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    person2 = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+    match_check=models.BooleanField(default = False)
+    created = models.DateTimeField(auto_now_add= True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable = False)
+
+    
