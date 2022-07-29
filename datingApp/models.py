c0b085f9e00bf234d53513d3c2983adcb3f43b07
Sarah-Rafi <sarahrafi123456@gmail.com>
2022-07-29 16:01:39 +0500

    testingcommit

--- datingApp/models.py
+++ datingApp/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.contrib.auth.models import User
 import uuid
 #HIII
+#helloo
 class SexualOrientation(models.Model):
     SEXUAL_ORIENTATION_CHOICES = (
     ('straight','STRAIGHT'),
