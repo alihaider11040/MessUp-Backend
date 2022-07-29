a226c24c87ea2b249274e0d168d5697334d32da6
alimusa2809 <ali.musa.28.09@gmail.com>
2022-07-29 11:22:04 +0500

    push forms.py for using django built in user.

--- messUp/datingApp/forms.py
+++ messUp/datingApp/forms.py
@@ -1,9 +1,17 @@
-from django import forms
-from .models import Profile 
+from dataclasses import fields
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.forms import ModelForm
 
-class ProfileForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
+
+class CustomProfileCreationForm(UserCreationForm):
     class Meta:
-        model = Profile
-       
-      
+        model = User
+        fields = ['first_name','email','username','password1','password2']
+        labels= {
+            'first_name':'Name'
+        }
+    def __init__(self, *args, **kwargs):
+        super(CustomProfileCreationForm, self).__init__(*args, **kwargs)
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
