a9bc60a61813795173e217f36234e6c98b7fad9d
Ali Haider <alihaider11040@gmail.com>
2022-08-01 13:07:39 +0500

    First API

--- messUp/datingApp/views.py
+++ messUp/datingApp/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+@api_view(['POST'])
+def addwithphone(request): #send phone number and OTP i.e token generated for authecation
+    data = request.data()
+    phoneNumber=data['phoneNumber']
+    alreadyExists = Login.objects.filter(phone_number=path).exists()
+    if alreadyExists:
+        content = {'detail': 'user already exist!'}
+        return Response(content)
+    else:
+        serializer = Userbymobileserializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data[], status=status.HTTP_201_CREATED)
+
+
+
