0fabeab3879937f32a8fe79bd0c1a59ea0f2b80b
Ali Haider <alihaider11040@gmail.com>
2022-08-02 12:31:25 +0500

    Update views.py

--- messUp/datingApp/views.py
+++ messUp/datingApp/views.py
@@ -5,6 +5,23 @@ from ..datingApp.serializers import ProfileSerializer
 from datingApp.models import Profile, Profession, Zodiac, Login, Interests, SexualOrientation, Institute
 
 
+
+@api_view(['POST'])
+def addwithphone(request): #send phone number and OTP i.e token generated for authecation
+    data = request.data()
+    phoneNumber=data['phoneNumber']
+    alreadyExists = Login.objects.filter(phone_number=phoneNumber).exists()
+    if alreadyExists:
+        content = {'detail': 'user already exist!'}
+        return Response(content)
+    else:
+        serializer = Userbymobileserializer(data=request.data)
+        if serializer.is_valid():
+            obj= serializer.save()
+            return Response(obj, status=status.HTTP_201_CREATED)
+
+
+
 @api_view(['GET'])
 def getRoutes(request):
 
