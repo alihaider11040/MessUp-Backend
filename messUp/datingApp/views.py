fa2d02e2c16f203bae91ee025718f9c612bd9489
alimusa2809 <ali.musa.28.09@gmail.com>
2022-08-03 18:35:32 +0500

    registered picture gallery model

--- messUp/datingApp/views.py
+++ messUp/datingApp/views.py
@@ -3,10 +3,9 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from rest_framework.request import Request
-from datingApp.serializers import ProfileSerializer, RegisterMatch, Userbymobileserializer, BlockProfile
+from datingApp.serializers import ProfileSerializer, RegisterMatch, Userbymobileserializer, BlockProfileSerializer
 from datingApp.models import Profile, Profession, Zodiac, Login, Interests, SexualOrientation, Institute
 from datingApp.models import MatchMake, BlockProfile
-from messUp.datingApp.serializers import BlockProfileSerializer
 
 
 
